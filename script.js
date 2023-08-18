c71b1d418cbd1856c46f78c3dfdbe3826953bbe5
Olesia Ivanova <ivanova.ov_87@mail.ru>
2023-08-18 21:17:14 +0300

    вычислене произведения числа

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello!");
+const num = 266219;
+let product = num.toString().split('').reduce((a, b) => a * b);
+console.log(product);
+let pow = product ** 3;
+console.log(pow);
+let res = pow.toString().substr(0, 2);
+console.log(res);
