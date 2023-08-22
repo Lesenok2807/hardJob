404e154ce79c0a20fc0c477889ea98338d4f48b2
Olesia Ivanova <73076953+Lesenok2807@users.noreply.github.com>
2023-08-22 22:18:47 +0300

    третий коммит 3 урока
    третий коммит 3 урока

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 'use strict';
 
-const ru = "Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье";
-const en = "Mondey, Tuesday, Wendnesday, Thudsday, Friday, Saturday, Sunday";
+const ru = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+const en = ['Mondey', 'Tuesday', 'Wendnesday', 'Thudsday', 'Friday', 'Saturday', 'Sunday'];
 let lang = ru;
 let namePerson = 'Александр'; 
 
 if (lang == ru) {
-    console.log(ru);
+    console.log(ru.join(', '));
 } else {
-    console.log(en);
+    console.log(en.join(', '));
 }
 
 switch (lang) {
     case ru:
-        console.log("Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье");
+        console.log(ru.join(', '));
         break;
     case en:
-        console.log("Mondey, Tuesday, Wendnesday, Thudsday, Friday, Saturday, Sunday");
+        console.log(en.join(', '));
         break;
     default:
         console.log("Что-то пошло не так");
 }
 
-lang === ru ? console.log(ru) : console.log(en);
+lang === ru ? console.log(ru.join(', ')) : console.log(en.join(', '));
 
 namePerson == 'Артем' ? console.log("директор") : console.log("студент"); 
-namePerson == 'Александр' ? console.log("преподаватель") : console.log("студент"); 
+namePerson == 'Александр' ? console.log("преподаватель") : console.log("студент");
